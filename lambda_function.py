64e188b5dfb3836db2f3c1a40c8dcaf7fc5964b4
aahm2221 <aisha.ahmed1@yahoo.com>
2023-10-22 16:48:04 -0500

    remove json loop

--- lambda_function.py
+++ lambda_function.py
@@ -2,6 +2,7 @@ import boto3
 import json
 import os
 from boto3.dynamodb.conditions import Key
+from dynamodb_json import json_util
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(os.environ['TABLE_NAME'])
@@ -15,4 +16,4 @@ def lambda_handler(event, context):
     teams = response['Items']
     for team in teams:
         team['TeamId']=int(team['TeamId'])
-    return {'statusCode': 200, 'body':json.dumps(teams)}
+    return {'statusCode': 200, 'body':json.dumps(json_util.loads(response['Items']))}
