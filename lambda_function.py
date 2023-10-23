33e37f1c5ae02f85d877a0887511762d659e37bd
Omar Ahmed <omarahmed1121@gmail.com>
2023-10-22 20:36:37 -0500

    cleanup

--- lambda_function.py
+++ lambda_function.py
@@ -5,23 +5,13 @@ from dynamodb_json import json_util
 
 def lambda_handler(event, context):
     try:
-        print("Executing Get Teams")
-
-        print("Connecting to DynamoDb...")
         dynamodb = boto3.resource('dynamodb')
-
-        print("Connecting to Table..")
         table = dynamodb.Table(os.environ['TABLE_NAME'])
-
-        print("Running teams query...")
         response = table.query(
             ProjectionExpression='TeamId,#n',
             KeyConditionExpression=Key('PK').eq('Team'),
             ExpressionAttributeNames = {'#n': 'Name'}
         )
-
-        print("Returning response...")
-        print(json_util.dumps(response['Items']))
         # return {
         #     'statusCode': 200,
         #     'body':json_util.dumps(response['Items'])
@@ -31,15 +21,8 @@ def lambda_handler(event, context):
             'body': json_util.loads(response['Items'])
         }
     except Exception as e:
-        print("Exception Found...")
-        print(e)
-    finally:
-        print('## ENVIRONMENT VARIABLES')
-        print(os.environ['AWS_LAMBDA_LOG_GROUP_NAME'])
-        print(os.environ['AWS_LAMBDA_LOG_STREAM_NAME'])
-        print(os.environ['TABLE_NAME'])
-        print('## EVENT')
-        print(event)
+        print(f'Exception: {e}')
+
     return {
             'statusCode': 500,
             'body': json_util.dumps('Invalid Request')
