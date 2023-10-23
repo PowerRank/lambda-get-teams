667b7499239557153af4e7f8ce179a0337902a3d
aahm2221 <aisha.ahmed1@yahoo.com>
2023-10-22 19:30:29 -0500

    add try except

--- lambda_function.py
+++ lambda_function.py
@@ -3,13 +3,21 @@ import os
 from boto3.dynamodb.conditions import Key
 from dynamodb_json import json_util
 
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table(os.environ['TABLE_NAME'])
-
 def lambda_handler(event, context):
-    response = table.query(
-        ProjectionExpression='TeamId,#n',
-        KeyConditionExpression=Key('PK').eq('Team'),
-        ExpressionAttributeNames = {'#n': 'Name'}
-    )
-    return {'statusCode': 200, 'body':json_util.loads(response['Items'])}
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(os.environ['TABLE_NAME'])
+    try:
+        response = table.query(
+            ProjectionExpression='TeamId,#n',
+            KeyConditionExpression=Key('PK').eq('Team'),
+            ExpressionAttributeNames = {'#n': 'Name'}
+        )
+        return {'statusCode': 200, 'body':json_util.loads(response['Items'])}
+    except:
+        print('Something went wrong')
+    finally:
+        print('## ENVIRONMENT VARIABLES')
+        print(os.environ['AWS_LAMBDA_LOG_GROUP_NAME'])
+        print(os.environ['AWS_LAMBDA_LOG_STREAM_NAME'])
+        print('## EVENT')
+        print(event)
