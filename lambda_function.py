3f1302430bc743002df23ba3956ea6d1f9077cba
oahmed-OS <omarahmed1121@gmail.com>
2023-10-22 21:15:32 -0500

    Resolve errors (#4)
    * remove region
    
    * remove body spacing
    
    * aisha version
    
    * add space
    
    * aisha two lines
    
    * move semicolon
    
    * cleanup
    
    * change to dumps
    
    * use old code
    
    * try out load
    
    * cleanup
    
    * move table in function
    
    * revert action back to main

--- lambda_function.py
+++ lambda_function.py
@@ -1,43 +1,26 @@
 import boto3
 import os
+import json
 from boto3.dynamodb.conditions import Key
-from dynamodb_json import json_util
+
+dynamodb = boto3.resource('dynamodb')
 
 def lambda_handler(event, context):
     try:
-        print("Executing Get Teams")
-
-        print("Connecting to DynamoDb...")
-        dynamodb = boto3.resource('dynamodb', region_name="us-east-1")
-
-        print("Connecting to Table..")
         table = dynamodb.Table(os.environ['TABLE_NAME'])
-
-        print("Running teams query...")
         response = table.query(
             ProjectionExpression='TeamId,#n',
             KeyConditionExpression=Key('PK').eq('Team'),
             ExpressionAttributeNames = {'#n': 'Name'}
         )
-
-        print("Returning response...")
-        print(json_util.dumps(response['Items']))
         return {
-            'statusCode': 200,
-            'body': json_util.dumps(response['Items'])
+            'statusCode': 200, 
+            'body': json.dumps(response['Items'])
         }
-        # return {'statusCode': 200, 'body':json_util.loads(response['Items'])}
     except Exception as e:
-        print("Exception Found...")
-        print(e)
-    finally:
-        print('## ENVIRONMENT VARIABLES')
-        print(os.environ['AWS_LAMBDA_LOG_GROUP_NAME'])
-        print(os.environ['AWS_LAMBDA_LOG_STREAM_NAME'])
-        print(os.environ['TABLE_NAME'])
-        print('## EVENT')
-        print(event)
+        print(f'Exception: {e}')
+
     return {
-            'statusCode': 200,
-            'body': json_util.dumps('Invalid Request')
+            'statusCode': 500,
+            'body': json.dumps('Invalid Request')
         }
