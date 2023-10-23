6195fb098027f3f7511d6bbaf7c4ca54222b521e
Omar Ahmed <omarahmed1121@gmail.com>
2023-10-22 20:27:55 -0500

    aisha version

--- lambda_function.py
+++ lambda_function.py
@@ -22,11 +22,11 @@ def lambda_handler(event, context):
 
         print("Returning response...")
         print(json_util.dumps(response['Items']))
-        return {
-            'statusCode': 200,
-            'body':json_util.dumps(response['Items'])
-        }
-        # return {'statusCode': 200, 'body':json_util.loads(response['Items'])}
+        # return {
+        #     'statusCode': 200,
+        #     'body':json_util.dumps(response['Items'])
+        # }
+        return {'statusCode': 200, 'body':json_util.loads(response['Items'])}
     except Exception as e:
         print("Exception Found...")
         print(e)
