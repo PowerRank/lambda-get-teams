a0c07a1f8123caab46baa5b24e1874567f8481dc
Omar Ahmed <omarahmed1121@gmail.com>
2023-10-22 20:50:39 -0500

    cleanup

--- lambda_function.py
+++ lambda_function.py
@@ -2,30 +2,25 @@ import boto3
 import os
 import json
 from boto3.dynamodb.conditions import Key
-from dynamodb_json import json_util
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(os.environ['TABLE_NAME'])
 
 def lambda_handler(event, context):
     try:
-        dynamodb = boto3.resource('dynamodb')
-        table = dynamodb.Table(os.environ['TABLE_NAME'])
         response = table.query(
             ProjectionExpression='TeamId,#n',
             KeyConditionExpression=Key('PK').eq('Team'),
             ExpressionAttributeNames = {'#n': 'Name'}
         )
-        # return {
-        #     'statusCode': 200,
-        #     'body':json_util.dumps(response['Items'])
-        # }
-        teams = json_util.loads(response['Items'])
         return {
             'statusCode': 200, 
-            'body': json.dumps(teams)
+            'body': json.dumps(response['Items'])
         }
     except Exception as e:
         print(f'Exception: {e}')
 
     return {
             'statusCode': 500,
-            'body': json_util.dumps('Invalid Request')
+            'body': json.dumps('Invalid Request')
         }
